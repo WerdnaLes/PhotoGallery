1c4bbcf203468688ba946007ec27125dcd5dcfbf
WerdnaLes <werdnales@gmail.com>
2022-10-25 19:46:54 +0300

    Working on chap 20(Network requests). Added setup for Retrofit fetches.

---
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
@@ -0,0 +1,60 @@
+package com.bignerdranch.android.photogallery
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.GridLayoutManager
+import com.bignerdranch.android.photogallery.api.FlickrApi
+import com.bignerdranch.android.photogallery.databinding.FragmentPhotoGalleryBinding
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.create
+
+class PhotoGalleryFragment : Fragment() {
+
+    private var _binding: FragmentPhotoGalleryBinding? = null
+    private val binding
+        get() = checkNotNull(_binding) {
+            "Cannot access binding because it is null. Is the view visible?"
+        }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding =
+            FragmentPhotoGalleryBinding.inflate(inflater, container, false)
+        binding.photoGrid.layoutManager =
+            GridLayoutManager(context, 3)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Using the Retrofit object to create an instance of the API
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://www.flickr.com/")
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .build()
+
+        val flickrApi =
+            retrofit.create<FlickrApi>()
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            val response = flickrApi.fetchContents()
+            Log.d("PhotoGalleryFragment", "Response received: $response")
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
